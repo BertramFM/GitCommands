2c61296e25fb94b5487b4da553f3ea0593fbf72c
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-03-19 12:02:07 +0100

    does not work

--- src/main/java/dk/ek/Main.java
+++ src/main/java/dk/ek/Main.java
@@ -2,12 +2,6 @@ package dk.ek;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Dav");
-
-        // Works
+public class Mai
     }
 }
